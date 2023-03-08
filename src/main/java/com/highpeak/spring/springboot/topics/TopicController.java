58fe2cbc9584876d8a79571c951db2e09b0459c5
Faizgk <gkfaiz123@gmail.com>
2023-03-08 15:56:37 +0530

    first commit

---
+++ src/main/java/com/highpeak/spring/springboot/topics/TopicController.java
@@ -0,0 +1,38 @@
+package com.highpeak.spring.springboot.topics;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+    @RequestMapping("/topics")
+    public List<Topic> getTopics() {
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/getTopic{id}")
+    public Optional<Topic> getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/updateTopic{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/deleteTopic{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
+    }
+}
---
+++ src/main/java/com/highpeak/spring/springboot/topics/TopicRepository.java
@@ -0,0 +1,10 @@
+package com.highpeak.spring.springboot.topics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TopicRepository extends JpaRepository<Topic, String> {
+
+
+
+}
